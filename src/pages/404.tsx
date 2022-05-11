0062b4a65a790b582a79f39aff2c0d005f4ec6a6
Ton Almeida <ton@tonalmeida.dev>
2022-05-10 23:07:38 -0300

    Add página 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,25 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
 export default function FourOhFour() {
   return (
-    <div>
-      <h1>404</h1>
-      <span>ok</span>
-    </div>
+    <>
+      <Head>
+        <title>Página não encontrada</title>
+      </Head>
+      <div className="container">
+        <div className="content">
+          <h1 className="title">404 | Página não encontrada</h1>
+          <span className="branding">
+            Voltar para o&nbsp;
+            <Link href="/">
+              <a target="_blank" rel="noopener noreferrer">
+                Início
+              </a>
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
   )
 }
